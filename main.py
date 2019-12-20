8f7a8ffc0f5e87981f901b18ad788098b4deef0b
binkagome <maobinfu@gmail.com>
2019-12-20 11:18:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 def main():
 #    file_read()
     
-    addr_latlng_domino = get_lat_lon_from_address(addr_list_domino)
-    addr_latlng_pizzahut = get_lat_lon_from_address(addr_list_pizzahut)
+    addr_latlng_1 = get_lat_lon_from_address(addr_list_1)
+    addr_latlng_2 = get_lat_lon_from_address(addr_list_2)
     with open('output.csv', 'w',newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["Domino", "Pizzahut", "distance"])
+        writer.writerow(["1", "2", "distance"])
         
-        for addr0 in addr_latlng_domino.keys():
-            for addr1 in addr_latlng_pizzahut.keys():
-                pos0 = tuple(np.array(addr_latlng_domino[addr0], dtype=float))
-                pos1 = tuple(np.array(addr_latlng_pizzahut[addr1], dtype=float))
+        for addr0 in addr_latlng_1.keys():
+            for addr1 in addr_latlng_2.keys():
+                pos0 = tuple(np.array(addr_latlng_1[addr0], dtype=float))
+                pos1 = tuple(np.array(addr_latlng_2[addr1], dtype=float))
                 
                 row = [addr0,  addr1, dist_on_sphere(pos0, pos1)]
                 writer.writerow(row)
